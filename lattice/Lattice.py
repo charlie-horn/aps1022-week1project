61658336568a1b394e4fa3787b5dee61bebc2d98
TurtleJeff <jefflundyx@gmail.com>
2021-06-03 03:35:39 -0400

    add lattice boundary and plot

--- lattice/Lattice.py
+++ lattice/Lattice.py
@@ -1,4 +1,4 @@
-from math import exp, sqrt, comb
+from math import exp, sqrt, comb, inf
 
 class Lattice:
     def __init__(self, risk_free_rate=0.02, current_price=100, volatility=0.25, strike_price=105, maturity = 2/12, num_steps = 8):
@@ -27,7 +27,8 @@ class Lattice:
         weights = []
 
         for i in range(2**self.m):
-            events = '{:08b}'.format(i)
+            conversion = '{:0'+str(self.m)+'b}'
+            events = conversion.format(i)
             S = [self.S_0]
             weight = 1
             for char in events:
@@ -70,6 +71,7 @@ class Lattice:
         return
 
     def american_put_pricing(self):
+        # step 1: calculate the stock price at each node
         stock_prices = [[self.S_0]]  # the stock price of all possible time spots
 
         for time_step in range(1, self.m+1):
@@ -82,7 +84,11 @@ class Lattice:
                     prices_temp.append(stock_prices[time_step - 1][event // 2] * self.d)
             stock_prices.append(prices_temp)
 
+        # step 2: calculate the option price/investor decision at each node
         option_prices = [] # the option price of all possible time spots
+        decisions = [['E']*2**self.m] # the optimal decision at each node, 'H' for holding, 'E' for execution
+        max_execution = [-inf]*self.m
+        min_holding = [inf]*self.m
 
         # when m = T, the price is the payoff
         prices_temp = []
@@ -93,11 +99,52 @@ class Lattice:
         # otherwise, the price is the max of payoff at m or the PV of expected price at m+1,
         for time_step in reversed(range(0, self.m)):
             prices_temp = []
+            decisions_temp = []
             num_events = 2**time_step
             for event in range(num_events):
                 execution_profit = max(0, self.K - stock_prices[time_step][event])
                 holding_profit = exp(-self.r*self.T/self.m) * (self.p*option_prices[0][2*event] + (1-self.p)*option_prices[0][2*event+1])
                 prices_temp.append(max(execution_profit, holding_profit))
-            option_prices.insert(0, prices_temp)
+                if holding_profit - execution_profit > -0.0001:
+                    decisions_temp.append('H')
+                    min_holding[time_step] = min(stock_prices[time_step][event], min_holding[time_step])
+                else:
+                    decisions_temp.append('E')
+                    max_execution[time_step] = max(stock_prices[time_step][event], max_execution[time_step])
 
-        print("--- American Put: {}".format(option_prices[0][0]))
+            option_prices.insert(0, prices_temp)
+            decisions.insert(0, decisions_temp)
+
+        # first element is the initial value, meaningless
+        del min_holding[0]
+        del max_execution[0]
+
+        # calculate the average optimal stopping times
+        weights = []
+        stopping_times = []
+
+        for i in range(2**self.m):
+            conversion = '{:0'+str(self.m)+'b}'
+            events = conversion.format(i)
+            weight = 1
+            decision_list = []
+            for time in range(self.m):
+                event = int(events[0:time+1], 2)
+                decision_list.append(decisions[time+1][event])
+                if events[time] == '1':
+                    weight *= self.p
+                else:
+                    weight *= 1-self.p
+            weights.append(weight)
+            stopping_times.append(decision_list.index('E')+1)
+
+        average_optimal_time = sum([weights[i] * stopping_times[i] for i in range(2**self.m)])
+        average_optimal_time2 = sum(stopping_times)/len(stopping_times)
+
+
+        print("--- American Put Price: {}".format(option_prices[0][0]))
+        print("--- American Put Average Stopping Time (Actual Weights): {}".format(average_optimal_time))
+        print("--- American Put Average Stopping Time (Equal Weights): {}".format(average_optimal_time2))
+
+        return min_holding, max_execution
+
--- lattice/main.py
+++ lattice/main.py
@@ -1,9 +1,36 @@
 from Lattice import  Lattice
-testing = Lattice(risk_free_rate=0.02,
+import matplotlib.pyplot as plt
+
+project = Lattice(risk_free_rate=0.02,
                   current_price=100,
                   volatility=0.25,
                   strike_price=105,
                   maturity = 2/12,
                   num_steps = 8)
-testing.euro_option_pricing()
-testing.american_put_pricing()
+project.euro_option_pricing()
+holding_boundary, execution_boundary = project.american_put_pricing()
+
+# make a plot
+x1 = [i for i in range(1,8)]
+x2 = [i for i in range(3,8)]
+
+fig, ax = plt.subplots()
+fig.set_size_inches(9, 6)
+ax.plot(x1, holding_boundary)
+ax.plot(x2, execution_boundary[2:])
+ax.set(xlabel='time (steps)',
+       ylabel='asset price (USD)',
+       xlim=[0, 8],
+       ylim=[85, 115])
+ax.legend(['Holding', 'Exercising'])
+ax.grid()
+plt.show()
+
+# # this is the example of the paper: price: 12.8618; stopping time: 3.25
+# example = Lattice(risk_free_rate=0.1,
+#                   current_price=100,
+#                   volatility=0.34641,
+#                   strike_price=110,
+#                   maturity = 4/12,
+#                   num_steps = 4)
+# example.american_put_pricing()
